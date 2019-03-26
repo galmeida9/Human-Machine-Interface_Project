b288c0d4de64e51284294e86a256e4a6d8c76515
Gabriel Almeida <galmeida9@gmail.com>
2019-03-26 18:22:52 +0000

    changed z index of sidebar

--- prototipo/js/sidebar.js
+++ prototipo/js/sidebar.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("menu").style.width = "113px";
+    document.getElementById("menu").style.zIndex = 4;
   }
   
 function closeNav() {
     document.getElementById("menu").style.width = "0";
+    document.getElementById("menu").style.zIndex = 2;
 }
