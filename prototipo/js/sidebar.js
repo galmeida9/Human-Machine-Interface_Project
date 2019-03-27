498eedc243a6635b6d350334ee23d21b957d70bb
Gabriel Almeida <galmeida9@gmail.com>
2019-03-27 14:04:32 +0000

    added news page

--- prototipo/js/sidebar.js
+++ prototipo/js/sidebar.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("menu").style.width = "113px";
-    document.getElementById("menu").style.zIndex = 4;
+    document.getElementById("menu").style.zIndex = 10;
   }
   
 function closeNav() {
